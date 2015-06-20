20effbd321e31645880d47803cf13de4076e1794
Lukas Rose <lukas.rose@gmx.de>
2015-06-20 11:19:17 +0200

    Basic NetworkClient implemented

--- src/network/NetworkClient.java
+++ src/network/NetworkClient.java
@@ -1,15 +1,49 @@
 package network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import logging.Logger;
+
 //connects to a listening socket
 
-public class NetworkClient extends Thread {
+public class NetworkClient {
 	private int port;
-	public NetworkClient(int port){
+	private String hostname;
+	private NetworkNotifier notifier;
+	private Socket socket;
+	private PrintWriter out;
+	private BufferedReader in;
+	private boolean verbose = true;
+	
+	public NetworkClient(NetworkNotifier notifier, String hostname, int port){
 		this.port = port;
+		this.hostname = hostname;
+		this.notifier = notifier;
+		try{
+			socket = new Socket (hostname, port);
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (UnknownHostException e){
+			Logger.logMessage('E', this, "Can't connect to host: " + hostname);
+			System.exit(1);		
+		} catch (IOException e){
+			Logger.logMessage('E', this, "Can't get I/O for the connection to " + hostname + " on port " + port);
+			System.exit(1);
+		}
 	}
 	
-	public void run(){
-		
+	public void send(String message){
+		if (out != null){
+			if (verbose) Logger.logMessage('I', this, "Sending message: " + message);
+			out.println(message);
+		} else {
+			Logger.logMessage('E', this, "Socket not established well, exiting.");
+			System.exit(1);
+		}
 	}
-	
 }
