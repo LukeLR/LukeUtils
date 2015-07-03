b9c406de9812708bcf8102a0042866f17d1ea63d
Lukas Rose <lukas.rose@gmx.de>
2015-07-04 01:33:24 +0200

    Added Log class Added Log class as a wrapper class for individual logs that saves the Log StringBuilder as well as a channel name

---
+++ src/logging/Log.java
@@ -0,0 +1,40 @@
+package logging;
+
+public class Log {
+	public StringBuilder log;
+	public String channel = "default";
+	
+	public Log(){
+		log = new StringBuilder();
+		channel = "default";
+	}
+	
+	public Log(String channel){
+		log = new StringBuilder();
+		this.channel = channel;
+	}
+	
+	public void append(String str){
+		log.append(str);
+	}
+	
+	public void setChannel(String chnl){
+		channel = chnl;
+	}
+	
+	public String getChannel(){
+		return channel;
+	}
+	
+	public String toString(){
+		return "Log channel: " + channel + "\n" + log.toString();
+	}
+	
+	public StringBuilder getLog(){
+		return log;
+	}
+	
+	public boolean equals(Log compare){
+		return channel.equals(compare.getChannel());
+	}
+}
