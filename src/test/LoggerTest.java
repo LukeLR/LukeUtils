7521d223a7683ae6009fca33578af414646ffe2c
Lukas Rose <lukas.rose@gmx.de>
2015-07-05 14:35:03 +0200

    Created LoggerTest

--- src/test/LoggerTest.java
+++ src/test/LoggerTest.java
@@ -1,7 +1,14 @@
 package test;
 
+import logging.Logger;
+import logging.LogManager;
+
 public class LoggerTest {
 	public static void main(String[] args){
+		Logger.logMessage('I', new LoggerTest(), "Hello!", "default");
+		Logger.logMessage('I', new LoggerTest(), "Hallo!", "default");
+		Logger.logMessage('I', new LoggerTest(), "Ave!", "default");
 		
+		LogManager.saveLogFile("log.txt");
 	}
 }
